2ae020910a76170dcbc9715440f8ee995c43c933
suptronics <39259807+suptronics@users.noreply.github.com>
2020-10-28 10:46:05 +0800

    Update x930.sh

--- x930.sh
+++ x930.sh
@@ -1,4 +1,4 @@
-#x710 Powering on /reboot /shutdown from hardware
+#x930 Powering on /reboot /shutdown from hardware
 #!/bin/bash
 
     sudo sed -e '/shutdown/ s/^#*/#/' -i /etc/rc.local
@@ -15,7 +15,7 @@ echo "$BOOT" > /sys/class/gpio/export
 echo "out" > /sys/class/gpio/gpio$BOOT/direction
 echo "1" > /sys/class/gpio/gpio$BOOT/value
 
-echo "X710 Shutting down..."
+echo "X930 Shutting down..."
 
 while [ 1 ]; do
   shutdownSignal=$(cat /sys/class/gpio/gpio$SHUTDOWN/value)
@@ -26,24 +26,24 @@ while [ 1 ]; do
     while [ $shutdownSignal = 1 ]; do
       /bin/sleep 0.02
       if [ $(($(date +%s%N | cut -b1-13)-$pulseStart)) -gt $REBOOTPULSEMAXIMUM ]; then
-        echo "X710 Shutting down", SHUTDOWN, ", halting Rpi ..."
+        echo "X930 Shutting down", SHUTDOWN, ", halting Rpi ..."
         sudo poweroff
         exit
       fi
       shutdownSignal=$(cat /sys/class/gpio/gpio$SHUTDOWN/value)
     done
     if [ $(($(date +%s%N | cut -b1-13)-$pulseStart)) -gt $REBOOTPULSEMINIMUM ]; then 
-      echo "X710 Rebooting", SHUTDOWN, ", recycling Rpi ..."
+      echo "X930 Rebooting", SHUTDOWN, ", recycling Rpi ..."
       sudo reboot
       exit
     fi
   fi
-done' > /etc/x710pwr.sh
-sudo chmod +x /etc/x710pwr.sh
-sudo sed -i '$ i /etc/x710pwr.sh &' /etc/rc.local 
+done' > /etc/x930pwr.sh
+sudo chmod +x /etc/x930pwr.sh
+sudo sed -i '$ i /etc/x930pwr.sh &' /etc/rc.local 
 
 
-#X710 full shutdown through Software
+#X930 full shutdown through Software
 #!/bin/bash
 
     sudo sed -e '/button/ s/^#*/#/' -i /etc/rc.local
@@ -63,9 +63,9 @@ if ! [[ $SLEEP =~ $re ]] ; then
    echo "error: sleep time not a number" >&2; exit 1
 fi
 
-echo "X710 Shutting down..."
+echo "X930 Shutting down..."
 /bin/sleep $SLEEP
 
 echo "0" > /sys/class/gpio/gpio$BUTTON/value
-' > /usr/local/bin/x710softsd.sh
-sudo chmod +x /usr/local/bin/x710softsd.sh
+' > /usr/local/bin/x930softsd.sh
+sudo chmod +x /usr/local/bin/x930softsd.sh
